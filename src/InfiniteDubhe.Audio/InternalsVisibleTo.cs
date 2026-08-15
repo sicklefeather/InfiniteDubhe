3c5c0cc7f49c6b4623206c807bd37ea3916bf4fe
xiaoping <z_xiaoping@foxmail.com>
2026-08-15 20:16:30 +0800

    core+audio: 放行编辑器访问 Tween/Audio 内部（播放模式步进）

--- src/InfiniteDubhe.Audio/InternalsVisibleTo.cs
+++ src/InfiniteDubhe.Audio/InternalsVisibleTo.cs
@@ -1,3 +1,4 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("InfiniteDubhe.Engine")]
+[assembly: InternalsVisibleTo("InfiniteDubheEditor")]
--- src/InfiniteDubhe.Core/InternalsVisibleTo.cs
+++ src/InfiniteDubhe.Core/InternalsVisibleTo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("InfiniteDubhe.Engine")]
 [assembly: InternalsVisibleTo("InfiniteDubhe.Rendering")]
+[assembly: InternalsVisibleTo("InfiniteDubheEditor")]
